fddf28524f5872bb400a72ba085b1cbc790fc8ba
Satyam Kulkarni <punekar.satyam@gmail.com>
2022-01-17 04:20:24 +0530

    Minor Chnage

--- components/project.tsx
+++ components/project.tsx
@@ -13,7 +13,7 @@ const Project: FC<Props> = ({ title, image, width, height, children }) => {
 		<div className="w-full md:w-3/5 h-full flex flex-col md:flex-row justify-center items-center">
 			<div className="w-full md:w-1/3 h-full flex justify-center items-center">
 				{image && <Image src={image} width={width} height={height}></Image>}
-				{!image && <h1>Designing is hard! send help😥</h1>}
+				{!image && <h1>🚨Work in Progress🚨</h1>}
 			</div>
 			<div className="w-full md:w-2/3 h-full p-10">
 				<h1 className="text-2xl font-bold">{title}</h1>
